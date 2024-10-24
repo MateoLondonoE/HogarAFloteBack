54d588e56bf5b52d849e7131b631d3628ab47d15
PankoU963 <alejandro_franco82211@elpoli.edu.co>
2024-10-24 16:17:43 -0500

    Commit backend
    añadí en el controller, las funciones get de clientes por id y eliminar

--- Backend/controllers/cliente.controllers.js
+++ Backend/controllers/cliente.controllers.js
@@ -1,6 +1,7 @@
 import { pool } from '../db.js'
 
 export const registrarCliente = async (req, res) => {
+    console.log("hoasdfasdfas1")
     try {
         const {
             identificacion,
@@ -18,8 +19,20 @@ export const registrarCliente = async (req, res) => {
             password,
             rol,
         } = req.body;
+        
+        let tipo_usuario = 0
+        if(rol === 'cliente'){
+            tipo_usuario = 1
+        }
+        else{
+            tipo_usuario = 2
+        }
 
+
+        console.log("asdfasfekl2")
         console.log(req.body);
+        
+        console.log(rol + "holaaaaaaaaa")
 
         const result = await pool.query(`
             INSERT INTO public."Cliente"(
@@ -28,7 +41,7 @@ export const registrarCliente = async (req, res) => {
             [
                 usuario,
                 password,
-                rol,
+                tipo_usuario,
                 primerNombre,
                 segundoNombre,
                 apellidos,
@@ -65,4 +78,35 @@ export const getAllClientes = async (req, res) => {
     }
 }
 
+export const getClienteById = async(req, res)=>{
+    try {
+        const {id} = req.params
+        const result = await pool.query(`SELECT * FROM public."Cliente" WHERE id = $1`, [id])
+        
+        if(result.length > 0){
+            res.status(200).json(result.rows)
+        }
+        else{
+            res.status(404).json({message: "Cliente no encontrado"})
+        }
 
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const deleteClienteById = async(req, res)=>{
+    try {
+        const {id} = req.params
+        const result = await pool.query(`UPDATE public."Cliente" SET estado = False WHERE id = $1`, [id])
+
+        if(result.length > 0){
+            res.status(200).send({ message: "Cliente eliminado con éxito" });
+        }
+        else{
+            res.status(404).json({message: "Cliente no encontrado"})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
--- Backend/routes/cliente.routes.js
+++ Backend/routes/cliente.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllClientes, registrarCliente } from "../controllers/cliente.controllers.js";
+import { deleteClienteById, getAllClientes, getClienteById, registrarCliente } from "../controllers/cliente.controllers.js";
 
 const router = Router()
 
 router.post('/', registrarCliente)
 router.get('/', getAllClientes)
+router.get('/:id', getClienteById)
+router.delete('/:id', deleteClienteById)
 
 export default router;
